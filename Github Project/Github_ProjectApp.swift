10cc58f514e3bb3779d9b550abf228c679df9b02
Kelly Nie <kellywnie@gmail.com>
2024-07-26 13:09:08 -0400

    Initial Commit

---
+++ Github Project/Github_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ProjectApp.swift
+//  Github Project
+//
+//  Created by Kelly Nie on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
